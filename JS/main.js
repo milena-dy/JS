00d83b1217735818b38bb2fff17e757e2bb91115
milena-dy <106916622+milena-dy@users.noreply.github.com>
2022-06-29 10:19:39 -0300

    testes

---
+++ JS/main.js
@@ -0,0 +1,41 @@
+
+alert("Meu primeiro alerta usando JS");
+
+function botao(){
+    alert("Clicou")
+}
+/*var idade  = prompt("Qual a sua idade?")
+
+if (idade >= 18){
+    alert("Você é maior de idade!!!")
+} else{
+    alert("Você é menor de idade!!")
+}
+
+function soma(n1, n2){
+    return n1 + n2
+}
+
+alert (soma(5,5));
+
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome)
+}*/
+
+alert(setReplace("HTML é uma linguagem de programação!", "HTML", "JavaScript"))
+
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar!";
+}
+
+function redirecionar(){
+    window.open("https://github.com/milena-dy")
+}
+
+function mesmaPag(){
+    window.location.href = "https://github.com/milena-dy"
+}
+
+function mouse(){
+    document.getElementById(mouse).
+}
